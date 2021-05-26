7a721bad957df1cdc7d28e33dbc36cedf83f1840
Kate Su <kate.su@vc-technology.com>
2021-05-26 17:48:01 +0800

    week2 completed

--- Readme.mk
+++ Readme.mk
@@ -1,5 +1,5 @@
 # 六角學院 Vue 作品實戰班 2021夏季
 
 - [第一週 - 從函式拆解認識設計模式](https://katesu.github.io/vueTraining/week1/index.html)
-# - [第二週 - RESTful API 串接](https://katesu.github.io/vueTraining/week2/index.html)
-# - [第三週 - Vue.js 起步走](https://katesu.github.io/vueTraining/week3/index.html)
+- [第二週 - RESTful API 串接](https://katesu.github.io/vueTraining/week2/index.html)
+- [第三週 - Vue.js 起步走](https://katesu.github.io/vueTraining/week3/index.html)
--- week2/main.js
+++ week2/main.js
@@ -1,8 +1,84 @@
-const url = 'https://vue3-course-api.hexschool.io/';
-const path = 'lancelot'
+const apiUrl = 'https://vue3-course-api.hexschool.io/';
+const apiPath = 'lancelot';
 
-// login
-// check logged in
+const app = {
+  data: {
+    apiUrl: 'https://vue3-course-api.hexschool.io/',
+    apiPath: 'lancelot',
+    products: [],
+  },
+  checkLoggedIn() {
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
+      '$1'
+    );
 
-// get product list
-// delete product
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+    } else {
+      alert('請先登入');
+      document.location.href = 'index.html';
+    }
+  },
+  getProducts() {
+    const { apiUrl, apiPath } = this.data;
+
+    axios.get(`${apiUrl}api/${apiPath}/admin/products`).then((res) => {
+      this.data.products = res.data.products;
+      this.render();
+    });
+  },
+  deleteProduct(e) {
+    const { apiUrl, apiPath } = app.data;
+    const id = e.target.dataset.id;
+
+    axios.delete(`${apiUrl}api/${apiPath}/admin/product/${id}`).then((res) => {
+      const { success, message } = res.data;
+      if (success) {
+        app.getProducts();
+      } else {
+        alert(`${message}`);
+      }
+    });
+  },
+  render() {
+    const productList = document.getElementById('productList');
+    const productCount = document.getElementById('productCount');
+    const htmlStr = this.data.products
+      .map(
+        (item) => `
+          <tr>
+          <td>${item.title}</td>
+          <td width="120">${item.origin_price}</td>
+          <td width="120">${item.price}</td>
+          <td width="100">
+            <span class="">${item.is_enabled ? '啟用' : '未啟用'}</span>
+          </td>
+          <td width="120">
+            <button
+              type="button"
+              class="btn btn-sm btn-outline-danger move deleteBtn"
+              data-action="remove"
+              data-id="${item.id}"
+            >
+              刪除
+            </button>
+          </td>
+        </tr>
+      `
+      )
+      .join(' ');
+
+    productList.addEventListener('click', this.deleteProduct);
+    productList.innerHTML = htmlStr;
+    productCount.innerHTML = this.data.products.length;
+  },
+  created() {
+    this.checkLoggedIn();
+    this.getProducts();
+  },
+};
+
+window.onload = function () {
+  app.created();
+};
